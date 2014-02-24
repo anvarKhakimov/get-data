118b1ff12cdb6bd69d39d1ba161b9a296a164330
Anvar Khakimov <anvar.khakimov@frumatic.com>
2014-02-24 19:50:11 +0600

    proccesing :start & :end time and return result as object

--- app/app.js
+++ app/app.js
@@ -6,14 +6,13 @@ app.get('/', function(req, res){
 });
 
 app.get('/data/:start/:end', function(req, res){
-	console.log(req.route);
 	var start, end, result;
 
 	start = parseInt( req.route.params.start );
 	end = parseInt( req.route.params.end );
 
-	start = new Date(2014,2,20,11,33,0).getTime();
-	end = new Date(2014,2,21,13,33,0).getTime();
+	//start = new Date(2014,2,20,11,0,0).getTime();
+	//end = new Date(2014,2,20,11,0,0).getTime();
 
 	result = getDataByDate(start, end);
 	res.json(200, result)
@@ -23,20 +22,27 @@ app.listen(3000);
 
 
 function getDataByDate(start, end){
-	var diff, result = {};
+	var diff = end - start
+		, fullHours = new Date(start)
+		, isFullHours = new Date(start).getUTCMinutes() == 0
+		, result = {};
 
-	if( start >= end ) return result;
+	if( start > end ) return result;
 
-	var diff = end - start;
-
-	console.log(diff);
-
-	result[start] = [ Math.random() ];
-
-	for( var i = 0; i < Math.round(diff); i++ ){
-		result[i] = i;
-		console.log(i);
+	// lead to the next full hour
+	if( !isFullHours ) {
+		fullHours.setUTCMinutes(0)
+		fullHours.setUTCHours( new Date(start).getUTCHours() + 1 );
 	}
+
+	// (60*60*1000) - one hour
+	// @todo change results to 
+	for( var i = 0; i + fullHours.getTime() <= end; i += (60*60*1000) ){
+		if( i == 0 ) result[ new Date(start)] = start;
+		result[ new Date(i + fullHours.getTime()) ] = i;
+	}
+
+	if( !isFullHours ) result[ new Date(end) ] = end;
 
 	return result;
 }
